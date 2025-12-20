217f8aafac61e216562897bdf018116a25ba1369
Arapaimace <adarshv06.us@gmail.com>
2025-12-19 21:35:34 -0800

    some progress

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 def read(path: Path) -> Sentence:
+    output = []
     with open(path) as file:
         for line in file:
-            ...
+            word, part = line.split(',')
+            output.append(Word(word, part))
